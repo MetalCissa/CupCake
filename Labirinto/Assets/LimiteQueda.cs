f21fc8412fdf2f89e1b6a53d97df436e92ffc16b
Demétrio Menezes Neto <neto.demetrio@gmail.com>
2015-11-24 20:17:40 -0300

    Adicionando chão falso, todos thwomps movimentáveis agora são prefabs

---
+++ Labirinto/Assets/LimiteQueda.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimiteQueda : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other ){
+		if (other.gameObject.tag == "Player") {
+			other.GetComponent<Collider> ().isTrigger = false;
+			other.GetComponent<Transform> ().position = new Vector3 (-4.38f, 6.52f, 12.38f);
+		}
+	}
+}
