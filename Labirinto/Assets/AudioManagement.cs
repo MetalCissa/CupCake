f9f6a9fd1b54acd86e8f1ee3d91638eb476a7e63
Demétrio Neto <Demtrio Neto>
2015-11-28 19:07:10 -0300

    Footsteps

---
+++ Labirinto/Assets/AudioManagement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManagement : MonoBehaviour {
+
+	public AudioClip audioClip;
+
+	private Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		if (Input.GetAxis("Vertical") != 0.0f) {
+			if (!GetComponent<AudioSource> ().isPlaying)
+			GetComponent<AudioSource> ().Play ();
+		}
+	}
+}
