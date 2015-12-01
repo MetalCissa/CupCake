0f4690f8a9b2e5fe41c85ea0e4e215d3b6ddb6ac
Hamilton <Hamilton>
2015-12-01 11:04:49 -0300

    FINAL

--- Labirinto/Assets/Scripts/AtivarFim.cs
+++ Labirinto/Assets/Scripts/AtivarFim.cs
@@ -2,12 +2,34 @@
 using System.Collections;
 
 public class AtivarFim : MonoBehaviour {
-
+	public Camera camfim;
+	public GameObject nevoa; 
+	public GameObject luz;
 	public GameObject target;
+	private bool vai;
+	private float tempo;
 
+	void Start(){
+		tempo = 0;
+		vai = false;
+	}
+	void Update(){
+		tempo+= Time.deltaTime;
+		if(vai){
+
+			if(tempo >= 2){
+				target.SetActive(true);
+				camfim.enabled = true;
+				luz.GetComponent<Climatizacao>().activeScript = false;
+				nevoa.SetActive(false);
+			}
+
+		}
+	}
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
-			target.SetActive(true);
+			vai = true;
+			tempo = 0;
 		}
 	}
 }
---
+++ Labirinto/Assets/Scripts/MudarCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MudarCamera : MonoBehaviour {
+	public Camera cam1;
+	public Camera cam2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Tab)){
+			if(cam1.enabled){
+				cam1.enabled = false;
+				cam2.enabled = true;
+			}else{
+				cam1.enabled = true;
+				cam2.enabled = false;
+			}
+		}
+	
+	}
+}
